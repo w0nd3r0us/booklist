74194349753676739032c80df0b9129ad8c8eb4b
w0nd3r0us <steven.grewal@gmail.com>
2018-07-10 16:35:13 -0400

    worked on respond_to

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,19 @@ class BooksController < ApplicationController
 
   def index
     @books = books.all
+
+    respond_to do |format|
+      format.html
+      format.text
+
+      format.csv do
+        render plain: Book.generate_csv(@books)
+      end
+
+      format.json do
+        render json: @books
+      end
+    end
   end
 
 end
